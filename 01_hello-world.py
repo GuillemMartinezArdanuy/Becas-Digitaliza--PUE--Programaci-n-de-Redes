888446b2925d4b120e8025e244df29de39d48f71
Guillem Martinez Ardanuy <guillem.martinez.ardanuy@gmail.com>
2019-02-06 22:07:38 +0100

    1st Python exercice Becas Digitaliza - Programacion de Redes - Tutor: Iván Lago - Alumno: Guillem Martinez Ardanuy  - Nombre: 01_hello-world.py

---
+++ 01_hello-world.py
@@ -0,0 +1,13 @@
+'''
+Hello World
+------------------------------------------------------------------------------------------------------------
+
+Crear un archivo llamado 01_hello-world.py (a través del IDLE de Python o un IDE como, por ejemplo, Jupyter)
+Añadir la siguiente sentencia: print("Hello World"!)
+Guardar el script y ejecutarlo (corregir errores, si hubiese)
+Subir el archivo a vuestra cuenta de GitHub
+
+'''
+
+#   print("Hello World"!)    Error no finaliza con " antes de )           -> print("String")  or print('String')
+print("Hello World!")
