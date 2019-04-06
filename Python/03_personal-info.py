13eae735afb1f70e3a8381ee2e789f32f1ef6628
Guillem Martinez Ardanuy <47399195+GuillemMartinezArdanuy@users.noreply.github.com>
2019-04-06 13:18:50 +0200

    obtención de datos
    ^^

---
+++ Python/03_personal-info.py
@@ -0,0 +1,14 @@
+#Recoger datos del terminal
+#Crear un archivo llamado 03_personal-info.py
+#Crear un script que pregunte por el nombre, el apellido, el teléfono, la ciudad, y la edad. Cada valor ha de ser guardado en una variable diferente.
+nom = input("Como te llamas?")
+apellido1 = input ("Primer apellido? ")
+apellido2 = input("Segundo apellido? ")
+telefono = input ("Telefono? ")
+ciudad= input ("ciudad? ")
+edad = input ("edad?")
+
+
+#Imprimir, por pantalla, toda la infomarción coleccionada en una frase que combine la información con coherencia (una oración que no sólo sean los datos recogidos).
+print("Gracias ",nom,"!!! me gusta tu primer apellido:",apellido1,", tu segundo apellido no esta nada mal: ", apellido2, "\nEl cuarto numero de tu telefono es mi preferido, ", telefono,"\nno conozco tu ciudad, ",ciudad, "\nyo soy muy joven, acabo de nacer, tu tienes exactamente:",edad,"años \nHA SIDO UN PLACER OBTENER TUS DATOS ^^ ")
+#Subir el archivo a vuestra cuenta de GitHub.
