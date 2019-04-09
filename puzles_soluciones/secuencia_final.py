d54b49e504c390654aaca71c87b55ea23f1e9555
Guillem Martinez Ardanuy <47399195+GuillemMartinezArdanuy@users.noreply.github.com>
2019-04-09 12:34:06 +0200

    solucion_mia_sequencia
    en la app sololearn havia este reto:
    
    "identify the next two numbers in this sequence!":
    101,112,131,415,161,718,???,???
    
    he creado este algoritmo que creo que es una posible solución, 
    solo con una lista 
    
    crazy, or genius? xD ^^

---
+++ puzles_soluciones/secuencia_final.py
@@ -0,0 +1,21 @@
+
+lista=[1,1,2]
+
+def Unos_A_Los_Extremos(lista):
+    lista=[1,lista[-1]+1,1]
+    return lista
+
+
+
+def Uno_Al_Medio(lista):
+    lista=[lista[1]+1,1,lista[1]+2]
+    return lista
+
+print("lista uno al medio",Uno_Al_Medio(lista))
+
+iteraciones=int(input("number of iterations?"))
+for i in range (iteraciones):
+    lista=Unos_A_Los_Extremos(lista)
+    print(lista)
+    lista=Uno_Al_Medio(lista)
+    print(lista)
